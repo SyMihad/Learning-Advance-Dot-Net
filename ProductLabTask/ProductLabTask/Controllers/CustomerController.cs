790731dd6d72ec20af021b95d0f7e77e146aca96
SyMihad <symihad@gmail.com>
2023-10-29 00:43:06 +0600

    Product lab task version 1

---
+++ ProductLabTask/ProductLabTask/Controllers/CustomerController.cs
@@ -0,0 +1,172 @@
+﻿using ProductLabTask.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Script.Serialization;
+
+namespace ProductLabTask.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+        public ActionResult CreateCustomer()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CreateCustomer(Customer customer)
+        {
+            var db = new ProductEntities();
+            db.Customers.Add(customer);
+            db.SaveChanges();
+            return RedirectToAction("Login");
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(string email, string password)
+        {
+            var db = new ProductEntities();
+            var allCustomers = db.Customers.ToList();
+            var data = new Customer();
+            bool sucess = false;
+            foreach (var customer in allCustomers)
+            {
+                if(customer.Email == email && customer.Password == password)
+                {
+                    data = customer;
+                    sucess = true;
+                    Session["id"] = customer.id;
+                }
+            }
+
+            if (sucess)
+            {
+                return RedirectToAction("Home");
+            }
+            else
+            {
+                return RedirectToAction("Login");
+            }
+        }
+
+        public ActionResult Home()
+        {
+            return View();
+        }
+
+        public ActionResult ShowProducts()
+        {
+            var db = new ProductEntities();
+            var data = db.Products.ToList();
+            return View(data);
+        }
+
+        public ActionResult AddToCart(int id)
+        {
+            JavaScriptSerializer serializer = new JavaScriptSerializer();
+            HttpCookie ExistingCookie = Request.Cookies["CartCookie"];
+            if (ExistingCookie != null)
+            {
+                string cookieValue = ExistingCookie.Value;
+                List<int> oldValues = serializer.Deserialize<List<int>>(cookieValue);
+                oldValues.Add(id);
+                string sendList = serializer.Serialize(oldValues);
+                ExistingCookie.Value = sendList;
+                Response.Cookies.Add(ExistingCookie);
+            }
+            else
+            {
+                List<int> values = new List<int>();
+                values.Add(id);
+                string sendList = serializer.Serialize(values);
+                HttpCookie CartCookie = new HttpCookie("CartCookie");
+                CartCookie.Value = sendList;Response.Cookies.Add(CartCookie);
+            }
+            return RedirectToAction("ShowProducts");
+        }
+
+        public ActionResult ShowCart()
+        {
+            var db = new ProductEntities();
+            List<Product> allProducts = db.Products.ToList();
+            List<Product> cartProducts = new List<Product>();
+            JavaScriptSerializer serializer = new JavaScriptSerializer();
+            HttpCookie ExistingCookie = Request.Cookies["CartCookie"];
+            if (ExistingCookie != null)
+            {
+                string cookieValue = ExistingCookie.Value;
+                List<int> oldValues = serializer.Deserialize<List<int>>(cookieValue);
+                foreach(var product in allProducts)
+                {
+                    foreach(var id in oldValues)
+                    {
+                        if(product.id == id)
+                        {
+                            cartProducts.Add(product);
+                        }
+                    }
+                }
+                return View(cartProducts);
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        [HttpPost]
+        public ActionResult ConfirmOrders()
+        {
+            var db = new ProductEntities();
+            List<CustomerOrder> allOrders = new List<CustomerOrder>();
+            JavaScriptSerializer serializer = new JavaScriptSerializer();
+            HttpCookie ExistingCookie = Request.Cookies["CartCookie"];
+            if (ExistingCookie != null)
+            {
+                string cookieValue = ExistingCookie.Value;
+                List<int> oldValues = serializer.Deserialize<List<int>>(cookieValue);
+                foreach (int id in oldValues)
+                {
+                    CustomerOrder customerOrder = new CustomerOrder();
+                    customerOrder.PId = id;
+                    customerOrder.CId = (int)Session["id"];
+                    allOrders.Add(customerOrder);
+                }
+                foreach (CustomerOrder customerOrder in allOrders)
+                {
+                    db.CustomerOrders.Add(customerOrder);
+                }
+                db.SaveChanges();
+                return RedirectToAction("AllOrders");
+            }
+            else
+            {
+                return RedirectToAction("ShowProducts");
+            }
+        }
+
+        public ActionResult AllOrders()
+        {
+            var db = new ProductEntities();
+            List<CustomerOrder> customerOrders = new List<CustomerOrder>();
+            List<CustomerOrder> allOrders = db.CustomerOrders.ToList();
+            int customerId = (int)Session["id"];
+            foreach (var order in allOrders)
+            {
+                if(order.CId == customerId)
+                {
+                    customerOrders.Add(order);
+                }
+            }
+            return View(customerOrders);
+        }
+    }
+}
---
+++ ProductLabTask/ProductLabTask/Controllers/ProductController.cs
@@ -0,0 +1,86 @@
+﻿using ProductLabTask.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProductLabTask.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult AllProducts()
+        {
+            var db = new ProductEntities();
+            var data = db.Products;
+            return View(data);
+        }
+
+        [HttpGet]
+        public ActionResult createProduct()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult createProduct(Product product)
+        {
+            var db = new ProductEntities();
+            db.Products.Add(product);
+            db.SaveChanges();
+            return RedirectToAction("AllProducts");
+        }
+
+        [HttpGet]
+        public ActionResult EditProduct(int id)
+        {
+            var db = new ProductEntities();
+            var ex = (from d in db.Products where d.id == id select d).SingleOrDefault();
+            return View(ex);
+        }
+
+        [HttpPost]
+        public ActionResult EditProduct(Product product)
+        {
+            var db = new ProductEntities();
+            var exData = db.Products.Find(product.id);
+            exData.Name = product.Name;
+            exData.Price = product.Price;
+            exData.CatId = product.CatId;
+            db.SaveChanges();
+            return RedirectToAction("AllProducts");
+        }
+
+        public ActionResult DeleteProduct(int id)
+        {
+            var db = new ProductEntities();
+            var exData = db.Products.Find(id);
+            db.Products.Remove(exData);
+            db.SaveChanges();
+            return RedirectToAction("AllProducts");
+        }
+
+        public ActionResult AllCategories()
+        {
+            var db = new ProductEntities();
+            var data = db.Categories;
+            return View(data);
+        }
+
+        [HttpGet]
+        public ActionResult createCategory()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult createCategory(Category category)
+        {
+            var db = new ProductEntities();
+            db.Categories.Add(category);
+            db.SaveChanges();
+            return RedirectToAction("AllCategories");
+        }
+    }
+}
